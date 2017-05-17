b550c5ae33281a7f39a16fd5a7e23a71371be400
StuxCrystal <stuxcrystal@encode.moe>
2017-05-17 09:23:43 +0200

    Add target to gitignore

---
+++ src/main/java/de/rolimhf/schule/games/Hand.java
@@ -0,0 +1,16 @@
+package de.rolimhf.schule.games;
+
+/**
+ * This class contains every possible result
+ */
+public enum Hands {
+    ROCK,
+    PAPER,
+    SCISSORS,
+    LIZARD,
+    SPOCK;
+
+    Hands(Hands... others) {
+
+    }
+}
