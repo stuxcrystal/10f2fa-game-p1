056f6b691948ecebf72e47cfe5aaeaa5f07f1f18
StuxCrystal <stuxcrystal@encode.moe>
2017-05-17 08:46:01 +0200

    Adding some boilerplate code.

---
+++ src/main/java/de/rolimhf/schule/games/Main.java
@@ -0,0 +1,14 @@
+package de.rolimhf.schule.games;
+
+/**
+ * This is the main class of the game program.
+ *
+ * It will initialize its state and initiate the game.
+ */
+public class Main {
+
+    public static void main(String[] argv) {
+
+    }
+
+}
