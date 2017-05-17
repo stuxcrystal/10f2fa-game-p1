94f30829b0d12f597319d35294e927dfa1d38564
StuxCrystal <stuxcrystal@encode.moe>
2017-05-17 09:24:01 +0200

    Game done

--- src/main/java/de/rolimhf/schule/games/Hand.java
+++ src/main/java/de/rolimhf/schule/games/Hand.java
@@ -1,16 +1,72 @@
 package de.rolimhf.schule.games;
 
+import java.util.Random;
+
 /**
  * This class contains every possible result
  */
-public enum Hands {
+public enum Hand {
     ROCK,
     PAPER,
     SCISSORS,
     LIZARD,
     SPOCK;
 
-    Hands(Hands... others) {
+    // We need to set up the rules of the game beforehand.
+    //
+    // Since forward declarations are not supported, we are
+    // initializing its state in this block.
+    static {
+        ROCK.setSlays(SCISSORS, LIZARD);
+        PAPER.setSlays(SPOCK, ROCK);
+        SCISSORS.setSlays(LIZARD, PAPER);
+        LIZARD.setSlays(PAPER, SCISSORS);
+        SPOCK.setSlays(SCISSORS, ROCK);
+    }
 
+    /**
+     * Store an instance of an RNG here.
+     */
+    private static Random random = new Random();
+
+    /**
+     * Stores which hands should be slain.
+     */
+    private Hand[] slays;
+
+    /**
+     * This function sets which hands this hand slays.
+     *
+     * To prevent illegal forward references, we are defering
+     * to our own static block.
+     *
+     * @param hands The hands this hand slays.
+     */
+    private void setSlays(Hand... hands) {
+        this.slays = hands;
+    }
+
+    /**
+     * This method checks whether the hand is slaying the other hand.
+     *
+     * @param hand The hand
+     * @return {@code true} if the hand slays the other hand. {@code false} if not.
+     */
+    public boolean isSlaying(Hand hand) {
+        // Since we didn't learn sets yet we're just gonna
+        // iterate over our array.
+        for (Hand slays : this.slays) {
+            if (hand == slays) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns a random hand. This is the hand the computer will choose.
+     * @return The random hand the computer will play against the player.
+     */
+    public static Hand getRandomHand() {
+        int ordinal = random.nextInt(Hand.values().length);
+        return Hand.values()[ordinal];
     }
 }
--- src/main/java/de/rolimhf/schule/games/Main.java
+++ src/main/java/de/rolimhf/schule/games/Main.java
@@ -1,5 +1,7 @@
 package de.rolimhf.schule.games;
 
+import java.util.Scanner;
+
 /**
  * This is the main class of the game program.
  *
@@ -7,8 +9,62 @@ package de.rolimhf.schule.games;
  */
 public class Main {
 
+    /**
+     * Since we don't want to always create a new scanner instance,
+     * we are just gonna cache it here.
+     */
+    private static Scanner scanner;
+
+    /**
+     * Returns a single line with a prompt.
+     * @param prompt The prompt.
+     * @return The line the user entered.
+     */
+    static String getLine(String prompt) {
+        if (scanner == null) scanner = new Scanner(System.in);
+        if (prompt != null) System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    /**
+     * Queries the user for a hand.
+     *
+     * If the player mistypes, it will retry until the player chooses
+     * a valid hand.
+     *
+     * @return The hand the player chose.
+     */
+    static Hand getHandFromPlayer() {
+        while (true) {
+            String yourChoice = getLine("Your Choice: ");
+            try {
+                return Hand.valueOf(yourChoice.toUpperCase());
+            } catch (IllegalArgumentException ignored) {
+                System.out.println("This is not a valid hand. Try again.");
+            }
+        }
+    }
+
+    /**
+     * The actual game.
+     * @param argv
+     */
     public static void main(String[] argv) {
 
+        System.out.println("Welcome to Rock Paper Scissors Deluxe 2");
+
+        Hand yourHand = getHandFromPlayer();
+        Hand myHand = Hand.getRandomHand();
+
+        System.out.println(String.format("%s against %s", yourHand, myHand));
+
+        if (yourHand.isSlaying(myHand)) {
+            System.out.println("You win");
+        } else if (myHand.isSlaying(yourHand)) {
+            System.out.println("I win");
+        } else {
+            System.out.println("It's a draw");
+        }
     }
 
 }
